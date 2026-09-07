d25dd7563e9c1a0cf2857622f0b2fd886c5b6327
Jyrsinho <jyrgenhuhtala@gmail.com>
2026-09-07 15:22:38 +0300

    anekdoottien lataaminen palvelimelta tanStackQueryllä

--- query-anecdotes/src/App.jsx
+++ query-anecdotes/src/App.jsx
@@ -1,18 +1,42 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
+import { useQuery } from "@tanstack/react-query";
+
 
 const App = () => {
-  const handleVote = (anecdote) => {
-    console.log('vote')
-  }
+    const result = useQuery({
+        queryKey: ['anecdotes'],
+        queryFn: async () => {
+            const response = await fetch('http://localhost:3001/anecdotes');
+            if (!response.ok) {
+                throw new Error('Error fetching anecdotes');
+            }
+            return await response.json()
+        },
+        retry: 1
+    })
 
-  const anecdotes = [
-    {
-      content: 'If it hurts, do it more often',
-      id: '47145',
-      votes: 0,
-    },
-  ]
+    const handleVote = (anecdote) => {
+        console.log('vote')
+    }
+
+    if (result.isPending) {
+        return (
+            <div>
+                loading anecdotes...
+            </div>
+        )
+    }
+
+    if (result.isError) {
+        return (
+            <div>
+                anecdote service is unavailable due to an error in the server
+            </div>
+        )
+    }
+
+    const anecdotes = result.data
 
   return (
     <div>
